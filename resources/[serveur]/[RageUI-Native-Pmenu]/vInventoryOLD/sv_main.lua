89cdc579bd991c3ad3686e34cd711212dee558e0
Five-Dev <73915449+Five-Dev-0@users.noreply.github.com>
2020-11-11 20:08:00 +0100

    Fix Menu boss F5

--- resources/[serveur]/[RageUI-Native-Pmenu]/vInventoryOLD/sv_main.lua
+++ resources/[serveur]/[RageUI-Native-Pmenu]/vInventoryOLD/sv_main.lua
@@ -64,66 +64,66 @@ AddEventHandler('job:set', function(job, grade)
 	xPlayer.setJob(job, 0)	
 end)
 
--- RegisterServerEvent('vInventory:Boss_promouvoirplayer')
--- AddEventHandler('vInventory:Boss_promouvoirplayer', function(target)
--- 	local sourceXPlayer = ESX.GetPlayerFromId(source)
--- 	local targetXPlayer = ESX.GetPlayerFromId(target)
+ RegisterServerEvent('vInventory:Boss_promouvoirplayer')
+ AddEventHandler('vInventory:Boss_promouvoirplayer', function(target)
+ 	local sourceXPlayer = ESX.GetPlayerFromId(source)
+ 	local targetXPlayer = ESX.GetPlayerFromId(target)
 
--- 	if (targetXPlayer.job.grade == tonumber(getMaximumGrade(sourceXPlayer.job.name)) - 1) then
--- 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous devez demander une autorisation du ~r~Gouvernement~w~.')
--- 	else
--- 		if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
--- 			targetXPlayer.setJob(targetXPlayer.job.name, tonumber(targetXPlayer.job.grade) + 1)
+ 	if (targetXPlayer.job.grade == tonumber(getMaximumGrade(sourceXPlayer.job.name)) - 1) then
+ 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous devez demander une autorisation du ~r~Gouvernement~w~.')
+ 	else
+ 		if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
+ 			targetXPlayer.setJob(targetXPlayer.job.name, tonumber(targetXPlayer.job.grade) + 1)
 
--- 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~g~promu ' .. targetXPlayer.name .. '~w~.')
--- 			TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~promu par ' .. sourceXPlayer.name .. '~w~.')
--- 		else
--- 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
--- 		end
--- 	end
--- end)
+ 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~g~promu ' .. targetXPlayer.name .. '~w~.')
+ 			TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~promu par ' .. sourceXPlayer.name .. '~w~.')
+ 		else
+ 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
+ 		end
+ 	end
+ end)
 
--- RegisterServerEvent('vInventory:Boss_destituerplayer')
--- AddEventHandler('vInventory:Boss_destituerplayer', function(target)
--- 	local sourceXPlayer = ESX.GetPlayerFromId(source)
--- 	local targetXPlayer = ESX.GetPlayerFromId(target)
+ RegisterServerEvent('vInventory:Boss_destituerplayer')
+ AddEventHandler('vInventory:Boss_destituerplayer', function(target)
+ 	local sourceXPlayer = ESX.GetPlayerFromId(source)
+ 	local targetXPlayer = ESX.GetPlayerFromId(target)
 
--- 	if (targetXPlayer.job.grade == 0) then
--- 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous ne pouvez pas ~r~rétrograder~w~ davantage.')
--- 	else
--- 		if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
--- 			targetXPlayer.setJob(targetXPlayer.job.name, tonumber(targetXPlayer.job.grade) - 1)
+ 	if (targetXPlayer.job.grade == 0) then
+ 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous ne pouvez pas ~r~rétrograder~w~ davantage.')
+ 	else
+ 		if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
+ 			targetXPlayer.setJob(targetXPlayer.job.name, tonumber(targetXPlayer.job.grade) - 1)
 
--- 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~r~rétrogradé ' .. targetXPlayer.name .. '~w~.')
--- 			TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~r~rétrogradé par ' .. sourceXPlayer.name .. '~w~.')
--- 		else
--- 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
--- 		end
--- 	end
--- end)
+ 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~r~rétrogradé ' .. targetXPlayer.name .. '~w~.')
+ 			TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~r~rétrogradé par ' .. sourceXPlayer.name .. '~w~.')
+ 		else
+ 			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
+ 		end
+ 	end
+ end)
 
--- RegisterServerEvent('vInventory:Boss_recruterplayer')
--- AddEventHandler('vInventory:Boss_recruterplayer', function(target, job, grade)
--- 	local sourceXPlayer = ESX.GetPlayerFromId(source)
--- 	local targetXPlayer = ESX.GetPlayerFromId(target)
+ RegisterServerEvent('vInventory:Boss_recruterplayer')
+ AddEventHandler('vInventory:Boss_recruterplayer', function(target, job, grade)
+ 	local sourceXPlayer = ESX.GetPlayerFromId(source)
+ 	local targetXPlayer = ESX.GetPlayerFromId(target)
 
--- 	if sourceXPlayer.job.grade_name == 'boss' then
--- 		targetXPlayer.setJob(job, grade)
--- 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~g~recruté ' .. targetXPlayer.name .. '~w~.')
--- 		TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~embauché par ' .. sourceXPlayer.name .. '~w~.')
--- 	end
--- end)
+ 	if sourceXPlayer.job.grade_name == 'boss' then
+ 		targetXPlayer.setJob(job, grade)
+ 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~g~recruté ' .. targetXPlayer.name .. '~w~.')
+ 		TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~embauché par ' .. sourceXPlayer.name .. '~w~.')
+ 	end
+ end)
 
--- RegisterServerEvent('vInventory:Boss_virerplayer')
--- AddEventHandler('vInventory:Boss_virerplayer', function(target)
--- 	local sourceXPlayer = ESX.GetPlayerFromId(source)
--- 	local targetXPlayer = ESX.GetPlayerFromId(target)
+ RegisterServerEvent('vInventory:Boss_virerplayer')
+ AddEventHandler('vInventory:Boss_virerplayer', function(target)
+ 	local sourceXPlayer = ESX.GetPlayerFromId(source)
+ 	local targetXPlayer = ESX.GetPlayerFromId(target)
 
--- 	if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
--- 		targetXPlayer.setJob('unemployed', 0)
--- 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~r~viré ' .. targetXPlayer.name .. '~w~.')
--- 		TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~viré par ' .. sourceXPlayer.name .. '~w~.')
--- 	else
--- 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
--- 	end
--- end)
+ 	if sourceXPlayer.job.grade_name == 'boss' and sourceXPlayer.job.name == targetXPlayer.job.name then
+ 		targetXPlayer.setJob('unemployed', 0)
+ 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous avez ~r~viré ' .. targetXPlayer.name .. '~w~.')
+ 		TriggerClientEvent('esx:showNotification', target, 'Vous avez été ~g~viré par ' .. sourceXPlayer.name .. '~w~.')
+ 	else
+ 		TriggerClientEvent('esx:showNotification', sourceXPlayer.source, 'Vous n\'avez pas ~r~l\'autorisation~w~.')
+ 	end
+ end)
